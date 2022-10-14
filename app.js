3db228d1021419656434eeae22100e029c521d56
Antonin Crestois <antonin.crestois@gmail.com>
2022-10-14 17:16:37 +0800

    to anto's mongo yh code

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
 const express = require('express');
 const app = express();
+
 const https = require('https');
 const cookieParser = require('cookie-parser');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const MongoClient = require('mongodb').MongoClient;
 
+const MongoClient = require('mongodb').MongoClient;
 const CONNECTION_URL = "mongodb+srv://antomanto:nanostacko@nanostacko.ubay5wc.mongodb.net/test";
 const DATABASE_NAME = "nanostacko"; // you can change the database name
 var database, collection;
 
-MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-  if(error) throw error;
-
-  database = client.db(DATABASE_NAME);
-  collection = database.collection("newcollection"); // you can change the collection name
-});
 app.use(cookieParser());
+
 app.use(express.urlencoded({extended: true}));
+app.get("/", function(req, res){
+  res.render('home');
+});
 
-app.set("view engine","ejs");
-app.get('/', function(req, res){
-    res.render('home');
-  });
+app.set("view engine", "ejs");
 
-app.post('/',(req, res) => {
+app.post("/", function(req, res){
   collection.insertOne(req.body, (err, result) => {  
     if (err) return console.log(err)
 
     console.log('saved to database')
+    res.redirect('/') // or do something else here
   })
+
   function formv3(){
     // Create the new request 
     var xhr = new XMLHttpRequest();
-    var url = 'https://api.hsforms.com/submissions/v3/integration/submit/9381732/21cf36dc-c665-4656-912c-8a338a09253b';
+    var url = 'https://api.hsforms.com/submissions/v3/integration/submit/9381732/21cf36dc-c665-4656-912c-8a338a09253b'
+    
     // Example request JSON:
     var data = {
       "fields": [
@@ -53,7 +52,7 @@ app.post('/',(req, res) => {
     }
 
     var final_data = JSON.stringify(data)
-console.log(final_data);
+
     xhr.open('POST', url);
     // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -74,15 +73,20 @@ console.log(final_data);
     // Sends the request 
     
     xhr.send(final_data)
-    console.log('xhr runs');
  }
 
  formv3();
- console.log('formv3 has executed?');
- res.redirect("/") // or do something else here
-})
+});
+
+
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+  if(error) throw error;
+
+  database = client.db(DATABASE_NAME);
+  collection = database.collection("newcollection"); // you can change the collection name
 
   // Start the application after the database connection is ready
   app.listen(3000, () => {
-    console.log('This app is running on port 3000');
+    console.log('This app is running on port 3000')
   });
+});
